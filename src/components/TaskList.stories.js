51f03babcfef2b2a2a8bae0b449bbdabc746dd58
slobachev <sergeylobachev@outlook.com>
2019-10-23 17:36:52 +0300

    add state management

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
 
-import TaskList from "./TaskList";
-import { task, actions } from "./Task.stories";
-import Task from "./Task";
+import { PureTaskList } from './TaskList';
+import { task, actions } from './Task.stories';
 
 export const defaultTasks = [
-  { ...task, id: "1", title: "T1" },
-  { ...task, id: "2", title: "T2" },
-  { ...task, id: "3", title: "T3" },
-  { ...task, id: "4", title: "T4" },
-  { ...task, id: "5", title: "T5" },
-  { ...task, id: "6", title: "T6" }
+  { ...task, id: '1', title: 'Task 1' },
+  { ...task, id: '2', title: 'Task 2' },
+  { ...task, id: '3', title: 'Task 3' },
+  { ...task, id: '4', title: 'Task 4' },
+  { ...task, id: '5', title: 'Task 5' },
+  { ...task, id: '6', title: 'Task 6' },
 ];
 
 export const withPinnedTasks = [
   ...defaultTasks.slice(0, 5),
-  { id: "6", title: "T6 pinned", state: "TASK_PINNED" }
+  { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
 ];
 
-storiesOf("TaskList", module)
-  .addDecorator(story => <div style={{ padding: "3rem" }}>{story()}</div>)
-  .add("default", () => <TaskList tasks={defaultTasks} {...actions} />)
-  .add("withPinnedTasks", () => (
-    <TaskList tasks={withPinnedTasks} {...actions} />
-  ))
-  .add("loading", () => <TaskList loading tasks={[]} {...actions} />)
-  .add("empty", () => <TaskList tasks={[]} {...actions} />);
+storiesOf('TaskList', module)
+  .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
+  .add('default', () => <PureTaskList tasks={defaultTasks} {...actions} />)
+  .add('withPinnedTasks', () => <PureTaskList tasks={withPinnedTasks} {...actions} />)
+  .add('loading', () => <PureTaskList loading tasks={[]} {...actions} />)
+  .add('empty', () => <PureTaskList tasks={[]} {...actions} />);
